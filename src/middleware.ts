4766bbddaa4b9de97dd4bf82e32909ba3ad4771f
Abhishek Jaiswar <abhishekndxw@gmail.com>
2025-03-02 03:46:51 +0530

    feat: enhance middleware to skip authentication for additional public routes and images

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,28 +1,33 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-// Define explicitly public routes
 const isPublicRoute = createRouteMatcher([
-  "/", // Landing page is public
+  "/",
   "/sign-in(.*)",
   "/sign-up(.*)",
   "/api/workflows/(.*)*",
-  "/api/webhook/stripe"
+  "/api/webhook/stripe",
+  "/dashboard-light.png",
+  "/dashboard-dark.png",
 ]);
 
 export default clerkMiddleware(async (auth, req) => {
-  // Protect all routes except explicitly public ones
-  if (!isPublicRoute(req)) {
-    await auth.protect();
+  // Skip authentication for public routes & images
+  if (
+    !isPublicRoute(req) ||
+    req.nextUrl.pathname.startsWith("/public") ||
+    req.nextUrl.pathname.startsWith("/_next")
+  ) {
+    return;
   }
+
+  await auth.protect();
 });
 
 export const config = {
   matcher: [
     "/((?!_next|public|images|uploads|api/webhook/stripe|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-
-    // Protect specific sections
-    "/dashboard(.*)", // Protect dashboard pages
-    "/forum(.*)", // (Optional) Protect forum pages
-    "/api/trpc/(.*)", // Protect tRPC API routes
+    "/dashboard(.*)",
+    "/forum(.*)",
+    "/api/trpc/(.*)",
   ],
 };
