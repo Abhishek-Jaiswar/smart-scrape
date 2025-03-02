554fbbf863a5b30436b88b2338b8015204ede118
Abhishek Jaiswar <abhishekndxw@gmail.com>
2025-03-02 21:38:34 +0530

    refactor: remove dashboard route from middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,7 +9,6 @@ const isPublicRoute = createRouteMatcher([
   "/api/webhook/stripe",
   "/dashboard-light.png",
   "/dashboard-dark.png",
-  "/dashboard(.*)",
 ]);
 
 export default clerkMiddleware(async (auth, req) => {
